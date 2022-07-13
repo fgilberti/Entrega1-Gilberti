23f9dfd258ed43cb7ed993018e9768800dfcc3de
Francisco Gilberti <fgilberti60459@gmail.com>
2022-07-13 20:15:47 +0200

    En este momento se creo la AppVrental, los 2 templates de cada formulario, el archivo forms y se crearon los 3 modelos: Cliente, Vehiculo y Alquiler

--- AppVrental/views.py
+++ AppVrental/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def saludo(Request):
+    return HttpResponse("Bienvenido a V-Rental")
+
+def crear_estatico(request):
+    
+    objeto = Cliente( marca = "Mazda" , color = "Blanco" , cantidad_pasajeros = 5 , fecha_salida = "2021-11-13" )
+    objeto.save()
+    
+    return HttpResponse("Datos cargados con exito")
+
+
+    
