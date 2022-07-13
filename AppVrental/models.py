badd24067806c4900be2421e84d294451251599c
Francisco Gilberti <fgilberti60459@gmail.com>
2022-07-13 20:46:26 +0200

    Se creo los 3 modelos, vistas y formularios

--- AppVrental/models.py
+++ AppVrental/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.forms import DateField
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    edad = models.IntegerField()
+    dni = models.CharField(max_length=8)
+
+class Vehiculo(models.Model):
+    marca = models.CharField(max_length=20)
+    cant_asientos = models.IntegerField()
+    precio_dia = models.IntegerField()
+
+class Alquiler(models.Model):
+    fecha_retiro = DateField()
+    fecha_entrega = DateField()
+    
